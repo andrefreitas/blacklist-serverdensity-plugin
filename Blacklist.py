f496d376fd2b3fe2fc6c6aded2fc9ae477069f2a
André Freitas <p.andrefreitas@gmail.com>
2013-08-06 16:52:28 +0100

    Added GPL license information

--- Blacklist.py
+++ Blacklist.py
@@ -1,8 +1,21 @@
 """
 Blacklist Plugin for Server Density to monitor Email BlackList Listing
 Author: Andre Freitas
-Email: p.andrefreitas@gmail.com / andrefreitas@ptwebtech.pt
 Twitter: http://twitter.com/cantodoandre
+Linkedin: http://pt.linkedin.com/in/andrefreitas1/
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import os
